f360c1b35759768ce9c98abdbc15602a99c81a20
akanshagorivale <62110591+akanshagorivale@users.noreply.github.com>
2023-12-14 14:38:16 +0530

    Update testbucket.tf

--- resources/testbucket.tf
+++ resources/testbucket.tf
@@ -1,11 +1,5 @@
 # cloudResource.tf
 
-provider "aws" {
-  region = "ap-southeast-1"
-  # Other configuration options...
-}
-
-
 resource "aws_s3_bucket" "example" {
 provider = aws.bucket_region
 name = "testbucket"
