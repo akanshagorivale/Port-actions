34494a6a9ce12633f23672a11bdbe59c7fedf7e4
akanshagorivale <62110591+akanshagorivale@users.noreply.github.com>
2023-12-14 16:30:42 +0530

    Update cloudResource.tf

--- templates/cloudResource.tf
+++ templates/cloudResource.tf
@@ -1,24 +1,11 @@
 # cloudResource.tf
 
-variable "resource_prefix" {
-  description = "Prefix for resource name"
-  type        = string
+provider "aws" {
+  region = "ap-southeast-1"  # Replace with your desired AWS region
 }
 
-variable "resource_suffix" {
-  description = "Suffix for resource name"
-  type        = string
-}
-
-variable "resource_count" {
-  description = "Number of resources to create"
-  type        = number
-}
-
-resource "aws_s3_bucket" "example" {
-  provider = aws.bucket_region
-  count    = var.resource_count
-
-  name = "${var.resource_prefix}-example-${count.index + 1}-${var.resource_suffix}"
-  acl  = "{{ bucket_acl }}"
+resource "aws_s3_bucket" "example23" {
+provider = aws.bucket_region
+name = "{{ bucket_name }}"
+acl = "{{ bucket_acl }}"
 }
