b822599d383d1b269ba98b2e9bfb6adac5b95d43
akanshagorivale <62110591+akanshagorivale@users.noreply.github.com>
2023-12-16 08:20:56 +0530

    Update cloudResource.tf

--- templates/cloudResource.tf
+++ templates/cloudResource.tf
@@ -1,10 +1,6 @@
 # cloudResource.tf
 
-provider "aws" {
-  region = "ap-southeast-1"  # Replace with your desired AWS region
-}
-
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "{{ bucket_name }}" {
 provider = aws.bucket_region
 name = "{{ bucket_name }}"
 acl = "{{ bucket_acl }}"
