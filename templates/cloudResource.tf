5160ade6f99a70c43c0aa1727b4abcd3ecf0d7f7
akanshagorivale <62110591+akanshagorivale@users.noreply.github.com>
2023-12-22 11:01:16 +0530

    Update cloudResource.tf

--- templates/cloudResource.tf
+++ templates/cloudResource.tf
@@ -1,6 +1,6 @@
 # cloudResource.tf
 
-resource "aws_s3_bucket" "{{ bucket_name }}" {
+resource "aws_s3_bucket" "s3-bucket" {
 provider = aws.bucket_region
 name = "{{ bucket_name }}"
 acl = "{{ bucket_acl }}"
