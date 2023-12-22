b4e1390befab4a5b703b24a13182180ded106ce9
akanshagorivale <62110591+akanshagorivale@users.noreply.github.com>
2023-12-22 15:26:38 +0530

    Update cloudResource.tf

--- templates/cloudResource.tf
+++ templates/cloudResource.tf
@@ -1,7 +1,211 @@
 # cloudResource.tf
 
+resource "aws_vpc" "vpc_1" {
+
+  cidr_block = "172.26.0.0/16"
+  # enable_classiclink               = "false"
+  # enable_classiclink_dns_support   = "false"
+  enable_dns_hostnames = "true"
+  enable_dns_support   = "true"
+  instance_tenancy     = "default"
+
+  tags = {
+    Name    = "test-vpc-dec22"
+    
+  }
+
+  tags_all = {
+    Name    = "test-vpc-dec22"
+    
+  }
+}
+
+resource "aws_subnet" "public_sn1" {
+  vpc_id                  = aws_vpc.vpc_1.id
+  cidr_block              = "172.26.0.0/22"
+  availability_zone       = "ap-southeast-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name    = "public-subnet-dec22"
+    sprinto = var.tag_value
+  }
+}
+
+resource "aws_subnet" "private_sn1" {
+  vpc_id            = aws_vpc.vpc_1.id
+  cidr_block        = "172.26.4.0/22"
+  availability_zone = "ap-southeast-1a"
+  tags = {
+    Name    = "private-subnet-dec22"
+
+  }
+}
+
+
+
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc_1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name    = "public-route-table-dec22"
+
+  }
+}
+
+
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc_1.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "172.26.1.0"
+  }
+ 
+
+  tags = {
+    Name    = "private-route-table-dec22"
+
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  tags = {
+    Name    = "igw-dec22"
+
+  }
+}
+
+resource "aws_eip" "eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "natgw" {
+  subnet_id         = aws_subnet.public_sn1.id
+  allocation_id     = aws_eip.eip.id
+  private_ip        = var.nat_private_ip
+  connectivity_type = "public"
+
+  tags = {
+    Name    = "nat-gw-dec22"
+
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public_sn1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private_sn1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+
+
+
+resource "aws_network_acl" "public1-nacl" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name    = "public-subnet-nacl-dec22"
+
+  }
+}
+
+resource "aws_network_acl" "private1-nacl" {
+  vpc_id = aws_vpc.vpc_1.id
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name    = "private-subnet-nacl-dec22"
+
+  }
+}
+
+
+
+
+resource "aws_network_acl_association" "public1-nacl" {
+  network_acl_id = aws_network_acl.public1-nacl.id
+  subnet_id      = aws_subnet.public_sn1.id
+}
+
+resource "aws_network_acl_association" "private1-nacl" {
+  network_acl_id = aws_network_acl.private1-nacl.id
+  subnet_id      = aws_subnet.private_sn1.id
+}
+
+
+
 resource "aws_s3_bucket" "s3-bucket" {
-  bucket = "bucket-dec22"
+  bucket = "s3-bucket-dec22"
+}
+
+resource "aws_security_group" "lambda_sg" {
+  name        = "security-group-dec22"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc_1.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "security-group-dec22"
+  }
 }
 
 resource "aws_lambda_function" "lambda-function" {
@@ -28,8 +232,8 @@ resource "aws_lambda_function" "lambda-function" {
   }
 
   vpc_config {
-    subnet_ids         = ["subnet-0e0417ad12bcbd3c2"]
-    security_group_ids = ["sg-08a5d93965cb14f50"]
+    subnet_ids         = [aws_subnet.private_sn1.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
   }
 
 #  depends_on = [aws_iam_role.lambda-to-opensearch-role]
