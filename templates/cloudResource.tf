bd15dda75f0f38e01221c87ef007155f03be29fe
akanshagorivale <62110591+akanshagorivale@users.noreply.github.com>
2023-12-22 11:40:18 +0530

    Update cloudResource.tf

--- templates/cloudResource.tf
+++ templates/cloudResource.tf
@@ -2,5 +2,38 @@
 
 resource "aws_s3_bucket" "s3-bucket" {
   bucket = "bucket-dec22"
-  acl = "private"
+  acl    = "private"
+}
+
+resource "aws_lambda_function" "lambda-function" {
+  architectures = ["x86_64"]
+  description   = "An Amazon SQS trigger that logs messages in a queue."
+
+  ephemeral_storage {
+    size = "512"
+  }
+
+  function_name                  = "test-function-dec22"
+  filename                       = "lambda_function.zip"
+  handler                        = "index.handler"
+  memory_size                    = "128"
+  package_type                   = "Zip"
+  reserved_concurrent_executions = "-1"
+#  role                           = aws_iam_role.lambda-to-opensearch-role.arn
+  runtime                        = "nodejs16.x"
+
+  timeout = "3"
+
+  tracing_config {
+    mode = "PassThrough"
+  }
+
+  vpc_config {
+    subnet_ids         = ["subnet-0e0417ad12bcbd3c2"]
+    security_group_ids = ["sg-08a5d93965cb14f50"]
+  }
+
+#  depends_on = [aws_iam_role.lambda-to-opensearch-role]
+
+ 
 }
